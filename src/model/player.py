2b27ecf513c8046f8b1dba07a49ab79239ae86b1
rendon.luisegerardo@gmail.com <LuisGerardoRendon>
2021-10-05 17:35:47 -0500

    Se agregó base de datos remota

--- src/model/player.py
+++ src/model/player.py
@@ -16,21 +16,32 @@ class Player:
         self.player_id = None
 
     def instantiate_hashmap_to_register(self, hash_player_received: dict):
-        self.name= hash_player_received["name"]
+        self.name = hash_player_received["name"]
         self.gender = hash_player_received["gender"]
         self.age = hash_player_received["age"]
-        self.status = Status.ACTIVO
+        self.status = 1
         self.isModerator = False
         self.isVerified = False
         self.password = hash_player_received["password"]
         self.email = hash_player_received["email"]
 
-
     def sign_up(self) -> bool:
         registered = False
         conexion = EasyConnection()
-        query = "INSERT INTO player (nickname, email, password) VALUES (%s, %s, %s) ;"
-        values = [self.nickname, self.email, self.password]
+        query = "INSERT INTO player (name, gender, age, status, isModerator, isVerified, password, email) VALUES " \
+                "(%s, %s, %s, %s, %s, %s,  %s, %s);"
+        values = [self.name, self.gender, self.age, self.status, self.isModerator, self.isVerified, self.password,
+                  self.email]
         conexion.send_query(query, values)
         registered = True
         return registered
+
+    def get_id(self) -> int:
+        id_recuperado =-1
+        query = "SELECT player_id FROM player WHERE email = %s;"
+        values = [self.email]
+        conexion = EasyConnection()
+        resultado = conexion.send_query(query,values)
+        id_recuperado = resultado [0]["player_id"]
+        self.player_id = id_recuperado
+        return id_recuperado
--- src/services/RutasPlayer.py
+++ src/services/RutasPlayer.py
@@ -7,16 +7,21 @@ rutas_player = Blueprint("rutas_player", __name__)
 
 @rutas_player.route("/players", methods = ["POST"])
 def registrar_player():
+    print(request)
     player_recibido = request.json
     respuesta = Response(status=HTTPStatus.BAD_REQUEST)
-    valores_requeridos = {"email", "nickname", "password"}
+    valores_requeridos = {"name", "gender", "age", "email", "password"}
     if player_recibido is not None:
         if all(llave in player_recibido for llave in valores_requeridos):
             player = Player()
-            player.instantiate_hashmap(player_recibido)
+            player.instantiate_hashmap_to_register(player_recibido)
             if player.sign_up():
+                #player_id = player.get_id()
+
                 print("Player registrado!")
                 print(player_recibido["email"])
+                print("El id del player es: ")
+                #print( player_id)
                 respuesta = Response(status=HTTPStatus.OK)
             else:
                 respuesta = Response(status=HTTPStatus.INTERNAL_SERVER_ERROR)
